84b1108fca27b5da63ea77a10b41a3b36268bd59
Alistair Pattison <alistairpattison@gmail.com>
2025-03-17 20:08:05 -0400

    use ages through 84

--- lifetables/populations.py
+++ lifetables/populations.py
@@ -8,7 +8,7 @@ import polars.selectors as cs
 def get_standard_pops(
     url="https://seer.cancer.gov/stdpopulations/stdpop.singleages.html",
     age_column=cs.by_index(0),
-    pop_column=cs.by_index(1),
+    pop_column=cs.by_index(2), # single ages to 84
 ):
     raw = pl.from_pandas(pd.read_html(url).pop())
 
